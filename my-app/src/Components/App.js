1290de2d228f969e4359e3db746ec71464c3cac8
Loucil Rubio <mvr12@my.fsu.edu>
2018-03-11 16:08:58 -0400

    added switch routing

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AboutMe from './AboutMe/AboutMe'
 import Code from './Code/Code';
 import Design from './Design/Design';
@@ -15,14 +15,16 @@ class App extends Component {
   render() {
     return (
         <Router>
-            <div className="wrapper">
-                <Menu/>
-                <Route exact path="/" component={AboutMe} />
-                <Route path="/code" component={Code} />
-                <Route path="/illustrations" component={Illustrations} />
-                <Route path="/design" component={Design} />
-                <Route path="/misc" component={Misc} />
-            </div>
+            <Switch>
+                <div className="wrapper">
+                    <Menu/>
+                    <Route exact path="/" component={AboutMe} />
+                    <Route path="/code" component={Code} />
+                    <Route path="/illustrations" component={Illustrations} />
+                    <Route path="/design" component={Design} />
+                    <Route path="/misc" component={Misc} />
+                </div>
+            </Switch>
         </Router>
     );
   }
