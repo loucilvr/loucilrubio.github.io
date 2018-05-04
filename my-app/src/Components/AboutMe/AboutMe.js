dfe0b5cbcac088478176e5493d63d1646982e02f
Loucil Rubio <mvr12@my.fsu.edu>
2018-05-04 18:01:18 -0400

    updated style and about description

--- my-app/src/Components/AboutMe/AboutMe.js
+++ my-app/src/Components/AboutMe/AboutMe.js
@@ -1,5 +1,4 @@
 import React from  'react';
-import Footer from './../Footer/Footer';
 import './About.css'
 import Headshot from '../../assets/img/2016spring_headshot.png'
 import Menu from "../Menu/Menu";
@@ -12,12 +11,15 @@ const AboutMe = () => {
            <div className="about">
                <div className="aboutHeader">
                    <img src={ Headshot } alt="hi" className="headshot"/>
-                <h1 className="name">hi there, i'm Loucil.</h1>
+                <h1 className="name">hi there, i'm Loucil</h1>
                    <div className="aboutDetails">
                        <p>
-                           Welcome to my online portfolio! I'm an <b>application developer with a passion for UI & UX Design.</b><br/><br/>
-                           I graduated from Florida State University with a Bachelor of Science degree
-                           in Information Technology in 2016, and jumpstarted my career in Tech at JPMorgan in Florida.
+                           I'm an <b>application developer with a passion for UI & UX Design. </b>
+                           How users interact with a product, or service, and what they experience are two of the things that
+                           hold the utmost importance to me as a developer & designer.<br/>
+                           <center>~</center>
+                           <br/>
+                           I graduated from Florida State University in 2016, and jumpstarted my career in Tech at JPMorgan Chase in Florida.
                            I built this portfolio to showcase my personal projects, share my current interests,
                            as well as my journey as a woman in technology.
                        </p>
@@ -28,7 +30,6 @@ const AboutMe = () => {
                    <span className="space"> | </span>
                    <a href="https://github.com/loucilvr">GITHUB</a>
                </div>
-               <Footer/>
            </div>
        </div>
    );
--- my-app/src/Components/Code/Code.js
+++ my-app/src/Components/Code/Code.js
@@ -8,16 +8,17 @@ const Code = ()=> (
         <div className="details content">
             <h3 className="name">Coding Projects</h3>
             <div className="projectDesc">
-                <h3>Flippy</h3>
-                <p>A simple React application for flipping through flash cards to help
-                    study for an exam.<br/>
+                <b>Flippy</b>
+                <p>A React application for flipping through flash cards to help
+                    study for an exam.
+                </p>
                     <button className="launchApp">
                         <a href="https://flippy-app.herokuapp.com/">Launch Flippy</a>
                     </button>
                 <br/>
                 <br/>
                 <br/>
-                Other projects will be added below in the coming weeks :)</p>
+                <p>Other projects will be added below in the coming weeks :)</p>
             </div>
         </div>
     </div>
