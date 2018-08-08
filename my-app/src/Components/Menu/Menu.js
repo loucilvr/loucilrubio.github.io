a6cd232d8dc2dff78ecc86dca2fb82480f47dea5
Loucil Rubio <mvr12@my.fsu.edu>
2018-08-08 19:25:56 -0400

    reorg menu links

--- my-app/src/Components/Menu/Menu.js
+++ my-app/src/Components/Menu/Menu.js
@@ -9,8 +9,8 @@ const Menu = () => {
             <div className="menu">
                 <Link to="/" className="menuLink">HOME</Link>
                 <Link to="/code" className="menuLink">CODE</Link>
-                <Link to="/illustrations" className="menuLink">ILLUSTRATIONS</Link>
                 <Link to="/design" className="menuLink">DESIGN</Link>
+                <Link to="/illustrations" className="menuLink">ILLUSTRATIONS</Link>
                 <Link to="/misc" className="menuLink">MISC</Link>
             </div>
         </div>
