8955cf6f749197efd42d7cd6e7e3da72e98208fb
Loucil Rubio <mvr12@my.fsu.edu>
2018-08-11 14:58:42 -0400

    remove unnecessary divs

--- my-app/src/Components/Code/Code.js
+++ my-app/src/Components/Code/Code.js
@@ -3,28 +3,26 @@ import './Code.css'
 import mockup1 from '../../assets/img/flippy-iphonex-mockup-1.png'
 import mockup2 from '../../assets/img/flippy-iphonex-mockup-2.png'
 
-const Code = ()=> (
-    <div>
-        <div className="details content">
-            <h3 className="name">Coding Projects</h3>
-            <div className="projectDesc">
-                <b>Flippy</b>
-                <p>A mobile-friendly web application for flipping through a set of study guide flash cards.
-                    Flippy was built with React
-                    and is currently deployed on Heroku.
-                </p>
-                <span className="showcaseApps">
-                    <img src={mockup2} alt="Flippy iPhone X mockup" width="150px"/>
-                    <img src={mockup1} alt="Flippy iPhone X mockup" width="150px"/>
-                </span>
-                <br/>
-                <br/>
-                <a href="https://flippy-app.herokuapp.com/">
-                    <button className="launchApp">
-                   Launch Flippy
-                    </button>
-                </a>
-            </div>
+const Code = () => (
+    <div className="details content">
+        <h3 className="name">Coding Projects</h3>
+        <div className="projectDesc">
+            <b>Flippy</b>
+            <p>A mobile-friendly web application for flipping through a set of study guide flash cards.
+                Flippy was built with React
+                and is currently deployed on Heroku.
+            </p>
+            <span className="showcaseApps">
+                <img src={mockup2} alt="Flippy iPhone X mockup" width="150px"/>
+                <img src={mockup1} alt="Flippy iPhone X mockup" width="150px"/>
+            </span>
+            <br/>
+            <br/>
+            <a href="https://flippy-app.herokuapp.com/">
+                <button className="launchApp">
+               Launch Flippy
+                </button>
+            </a>
         </div>
     </div>
 );
