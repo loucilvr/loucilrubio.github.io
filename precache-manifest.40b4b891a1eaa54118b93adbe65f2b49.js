3725bfbae3e852681699c6e4f02353387987f723
Loucil Rubio <mvr12@my.fsu.edu>
2019-04-09 10:37:20 -0400

    Updates

---
+++ precache-manifest.40b4b891a1eaa54118b93adbe65f2b49.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ad6fb171ae23fbf0fe2",
+    "url": "/static/css/main.662a3579.chunk.css"
+  },
+  {
+    "revision": "3ad6fb171ae23fbf0fe2",
+    "url": "/static/js/main.3ad6fb17.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4622fe9ed6a849d255fe",
+    "url": "/static/js/2.4622fe9e.chunk.js"
+  },
+  {
+    "revision": "a5a4310a89e1b561b3e2e037d27ba48d",
+    "url": "/static/media/2016spring_headshot.a5a4310a.png"
+  },
+  {
+    "revision": "ff10a5374db7207fbd3e979e3001c33b",
+    "url": "/static/media/flippy-iphonex-mockup-1.ff10a537.png"
+  },
+  {
+    "revision": "a803a0b8594ca93605b7d122007a100d",
+    "url": "/static/media/flippy-iphonex-mockup-2.a803a0b8.png"
+  },
+  {
+    "revision": "745c3e5f86782508b5488328ce880b3f",
+    "url": "/static/media/his-hers-breakfast.745c3e5f.jpg"
+  },
+  {
+    "revision": "8623c2ba925ca0e7c106ef80ceb5eff6",
+    "url": "/static/media/coffeeee.8623c2ba.jpeg"
+  },
+  {
+    "revision": "fa741dfc17445e555cd8b1d84f491bff",
+    "url": "/static/media/eucalyptus.fa741dfc.jpeg"
+  },
+  {
+    "revision": "e9961e965398422c8d249f706d58b446",
+    "url": "/index.html"
+  }
+];
