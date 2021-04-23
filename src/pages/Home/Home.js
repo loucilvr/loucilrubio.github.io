fbe9a1808f82d0c421d9e2d51ad19b8ea4e67478
Loucil <louvr67@gmail.com>
2021-04-23 01:03:50 -0400

    copy change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -78,14 +78,14 @@ const Home = () => {
             software at J.P. Morgan Chase, located in Tampa, FL.
             <br />
             <br />
-            This unique role I'm in now means I get to do both of the things
-            that I love and that is front end development and UI/UX design. So
-            on some days, I'm gathering user requirements, creating
-            high-fidelity designs on Sketch/Invision for our client-facing and
-            internal applications, or demoing and gathering feedback from
-            Product, tech and users. And on other days, I'm building UI
-            components in React, integrating screens with API services,
-            mentoring other engineers and shipping features into Production.
+            This role I'm in now allows me to do both of the things that I love
+            and that is front-end development and UI/UX design. So on some days,
+            I'm gathering user requirements, interviewing users, creating
+            low/high-fidelity designs on Sketch/Invision, or demoing and
+            gathering feedback from Product, tech and users. And on other days,
+            I'm building UI components in React, integrating screens with API
+            services and collaborating with other engineers to implement, deploy
+            and test our solutions.
             <br />
             <br />
             Outside of work, you'll find me painting, drawing, baking, playing
