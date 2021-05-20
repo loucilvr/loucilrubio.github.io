b63c664561675154693d9a43837b20751f9df449
Loucil <louvr67@gmail.com>
2021-05-20 15:42:41 -0400

    style enhancement to allow buttons to sit inline on smaller resolutions :)

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -6,7 +6,7 @@ const useStyles = createUseStyles({
   root: {
     fontWeight: "bold",
     borderRadius: "24px",
-    padding: "0 16px",
+    padding: "0 12px",
     height: "38px",
     fontSize: "14px",
     cursor: "pointer",
